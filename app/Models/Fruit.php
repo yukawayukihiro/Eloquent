36e4d031301a087388ecc850cb94904dd42fae73
yukawa <metalg9123@i.softbank.jp>
2023-04-24 20:27:10 +0900

    model作成

--- app/Models/Fruit.php
+++ app/Models/Fruit.php
@@ -3,8 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Fruit extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = ['name', 'color', 'price'];
 }
